7e1dc2af87b38570955c8a2ccbbfc0f5bd762787
dengol04 <dengol04@mail.ru>
2026-03-15 21:35:29 +0300

    dependencies form JAXB and postgres

--- StudentXmlImporter/build.gradle.kts
+++ StudentXmlImporter/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     kotlin("jvm") version "1.9.22"
 }
-
-group = "org.example"
+//
+//group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -11,6 +11,11 @@ repositories {
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    implementation("jakarta.xml.bind:jakarta.xml.bind-api:4.0.0")
+    implementation("org.glassfish.jaxb:jaxb-runtime:4.0.0")
+    
+    implementation("org.postgresql:postgresql:42.7.2")
 }
 
 tasks.test {
